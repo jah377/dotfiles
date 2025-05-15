2eed7d68df9254b5ae5488d668a1ec0db6fa4293
Jon <55328921+jah377@users.noreply.github.com>
2025-05-15 16:42:53 +0200

    Update osx.sh to only show active apps in dock

--- scripts/osx.sh
+++ scripts/osx.sh
@@ -60,3 +60,12 @@ defaults write com.apple.screencapture location -string "${SCREENSHOTS_PATH}"
 
 # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
 defaults write com.apple.screencapture type -string "png"
+
+
+###############################################################################
+# Dock                                                                        #
+###############################################################################
+
+# Only show active applications in dock
+defaults write com.apple.dock static-only -bool true
+killall Dock
