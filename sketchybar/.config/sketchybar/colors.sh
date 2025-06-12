ec9c23e05349b114eb49c86d7f742bf46b6dd594
jah377 <jonathan.harris.eit@gmail.com>
2025-06-12 16:59:45 +0200

    [skbar] Add colors.sh

---
+++ sketchybar/.config/sketchybar/colors.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# See: https://www.youtube.com/watch?v=8W06wMNZmo8
+
+# Make sure it's executable with:
+# chmod +x ~/.config/sketchybar/colors.sh
+
+export WHITE=0xffffffff
+export BAR_COLOR=0x40000000
+export ICON_COLOR=0xffffffff
+export LABEL_COLOR=0xffffffff
