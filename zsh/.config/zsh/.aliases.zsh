739de9db7e98719ff9d20f5f15027cbe2ab5fb25
jah377 <jonathan.harris.eit@gmail.com>
2025-09-16 19:06:05 +0200

    Refactor zsh/ directory

---
+++ zsh/.config/zsh/.aliases.zsh
@@ -0,0 +1,19 @@
+# Folders
+alias dow="$HOME/Downloads"
+alias doc="$HOME/dotfiles"
+
+# CLI tool shortcuts
+alias lg="lazygit"
+alias n="nvim"
+
+# Commands
+alias ls="eza -la --icons" # list, hidden, icons
+alias lst="eza -laT --icons" # list-tree, hidden, icons
+alias cd="z" # better `cd`
+
+# Directories
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ......="cd ../../../../.."
---
+++ zsh/.config/zsh/.vi-mode.zsh
@@ -0,0 +1,32 @@
+# Vim-motions in terminal
+# See https://github.com/hendrikmi/dotfiles/blob/main/zsh/custom.zsh
+bindkey -v
+export KEYTIMEOUT=1            # Makes switching modes quicker
+export VI_MODE_SET_CURSOR=true # trigger cursor shape changes when switching modes
+
+# Gets called every time the keymap changes (insert <-> normal mode)
+function zle-keymap-select {
+  if [[ ${KEYMAP} == vicmd ]]; then
+    echo -ne '\e[2 q' # block
+  else
+    echo -ne '\e[6 q' # beam
+  fi
+}
+zle -N zle-keymap-select # register as ZLE widget
+
+# Runs once when a new ZLE session starts (e.g. when a prompt appears)
+zle-line-init() {
+  zle -K viins # initiate 'vi insert' as keymap (can be removed if 'binkey -V has been set elsewhere')
+  echo -ne '\e[6 q'
+}
+zle -N zle-line-init
+echo -ne '\e[6 q' # Use beam shape cursor on startup
+
+# Yank to the system clipboard
+function vi-yank-xclip {
+  zle vi-yank
+  echo "$CUTBUFFER" | pbcopy -i
+}
+
+zle -N vi-yank-xclip
+bindkey -M vicmd 'y' vi-yank-xclip
