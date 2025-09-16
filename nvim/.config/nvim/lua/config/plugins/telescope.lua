c723bb96a18186e86e7cdf5a5af9582d53bc989f
jah377 <jonathan.harris.eit@gmail.com>
2025-09-16 19:05:39 +0200

    Fix telescope.find_files to include hidden files

--- nvim/.config/nvim/lua/config/plugins/telescope.lua
+++ nvim/.config/nvim/lua/config/plugins/telescope.lua
@@ -1,6 +1,3 @@
--- plugins/telescope.lua
--- Fuzzy finder for nvim
-
 return {
 	"nvim-telescope/telescope.nvim",
 	event = "VimEnter",
@@ -34,20 +31,25 @@ return {
 		vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "[F]ind [D]iagnostics" })
 		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[F]ind open [B]uffers" })
 		vim.keymap.set("n", "<leader>fp", builtin.git_files, { desc = "[F]ind git [P]roject files" })
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind directory [F]iles" })
 		vim.keymap.set("n", "<leader>ft", builtin.treesitter, { desc = "[F]ind [T]reesitter" })
 		vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "[F]ind [R]esume search" })
 		vim.keymap.set("n", "<leader>fz", builtin.current_buffer_fuzzy_find, { desc = "[F]ind fuz[Z]y in buffer" })
 
+		-- Search files, including hidden
+		vim.keymap.set("n", "<leader>ff", function()
+			builtin.find_files({ hidden = true })
+		end, { desc = "[F]ind directory [F]iles" })
+
 		vim.keymap.set("n", "<leader>fc", function()
-			builtin.find_files({ cwd = "~/dotfiles/" })
+			builtin.find_files({ cwd = "~/dotfiles/", hidden = true })
 		end, { desc = "[F]ind [C]onfig files" })
 
+		-- Search lazy package files
 		vim.keymap.set("n", "<space>fl", function()
 			builtin.find_files({ cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy") })
 		end, { desc = "[F]ind [L]azy files" })
 
-		-- Grep
+		-- Search using grep
 		vim.keymap.set("n", "<leader>fgs", builtin.grep_string, { desc = "[F]ind [G]rep for [S]tring" })
 		vim.keymap.set("n", "<leader>fgd", function()
 			builtin.live_grep({ grep_open_files = false, prompt_title = "Live Grep in Directory" })
