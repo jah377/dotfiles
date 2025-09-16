b58d5da4ff6ccaf1b6c201af527fd512c65f82ba
jah377 <jonathan.harris.eit@gmail.com>
2025-09-16 12:05:49 +0200

    Format wezterm.lua

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,53 +3,56 @@ local wezterm = require("wezterm")
 config = wezterm.config_builder()
 
 config = {
-automatically_reload_config = true,
-enable_tab_bar = false,
-window_close_confirmation = "NeverPrompt",
-window_decorations = "RESIZE", -- disable title bar; able to resize window
-window_padding = {left=5, right=5, top=0, bottom=0},
-default_cursor_style="SteadyBlock",
-color_scheme = "Catppuccin Macchiato (Gogh)",
+	automatically_reload_config = true,
+	enable_tab_bar = false,
+	window_close_confirmation = "NeverPrompt",
+	window_decorations = "RESIZE", -- disable title bar; able to resize window
+	window_padding = {
+		left = 10,
+		right = 10,
+		top = 10,
+		bottom = 10,
+	},
+	default_cursor_style = "SteadyBlock",
+	color_scheme = "Catppuccin Macchiato (Gogh)",
 
--- wezterm ls-fonts --list-system | grep "JetBrains" 
-font= wezterm.font("JetBrains Mono", {weight="Regular"}),
-font_size=14,
+	-- wezterm ls-fonts --list-system | grep "JetBrains"
+	font = wezterm.font("JetBrains Mono", { weight = "Regular" }),
+	font_size = 14,
 
--- from: https://akos.ma/blog/adopting-wezterm/
-hyperlink_rules = {
-	-- Matches: a URL in parens: (URL)
-	{
-		regex = "\\((\\w+://\\S+)\\)",
-		format = "$1",
-		highlight = 1,
+	-- from: https://akos.ma/blog/adopting-wezterm/
+	hyperlink_rules = {
+		-- Matches: a URL in parens: (URL)
+		{
+			regex = "\\((\\w+://\\S+)\\)",
+			format = "$1",
+			highlight = 1,
+		},
+		-- Matches: a URL in brackets: [URL]
+		{
+			regex = "\\[(\\w+://\\S+)\\]",
+			format = "$1",
+			highlight = 1,
+		},
+		-- Matches: a URL in curly braces: {URL}
+		{
+			regex = "\\{(\\w+://\\S+)\\}",
+			format = "$1",
+			highlight = 1,
+		},
+		-- Matches: a URL in angle brackets: <URL>
+		{
+			regex = "<(\\w+://\\S+)>",
+			format = "$1",
+			highlight = 1,
+		},
+		-- Then handle URLs not wrapped in brackets
+		{
+			regex = "[^(]\\b(\\w+://\\S+[)/a-zA-Z0-9-]+)",
+			format = "$1",
+			highlight = 1,
+		},
 	},
-	-- Matches: a URL in brackets: [URL]
-	{
-		regex = "\\[(\\w+://\\S+)\\]",
-		format = "$1",
-		highlight = 1,
-	},
-	-- Matches: a URL in curly braces: {URL}
-	{
-		regex = "\\{(\\w+://\\S+)\\}",
-		format = "$1",
-		highlight = 1,
-	},
-	-- Matches: a URL in angle brackets: <URL>
-	{
-		regex = "<(\\w+://\\S+)>",
-		format = "$1",
-		highlight = 1,
-	},
-	-- Then handle URLs not wrapped in brackets
-	{
-		regex = "[^(]\\b(\\w+://\\S+[)/a-zA-Z0-9-]+)",
-		format = "$1",
-		highlight = 1,
-	},
-}
-
-
 }
 
 return config
