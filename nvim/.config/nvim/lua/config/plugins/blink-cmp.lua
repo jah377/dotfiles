56af46b1bbcb24e0a205a56f89fcb7cedab2701c
jah377 <jonathan.harris.eit@gmail.com>
2025-09-22 17:03:57 +0200

    Add keymaps to blink-cmp.lua

--- nvim/.config/nvim/lua/config/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/config/plugins/blink-cmp.lua
@@ -18,7 +18,22 @@ return {
     -- C-k: Toggle signature help (if signature.enabled = true)
     --
     -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = { preset = "default" },
+    keymap = {
+      preset = "none",
+
+      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+      ["<C-e>"] = { "hide", "fallback" },
+      ["<C-y>"] = { "select_and_accept", "fallback" },
+      ["<Up>"] = { "select_prev", "fallback" },
+      ["<Down>"] = { "select_next", "fallback" },
+      ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+      ["<C-n>"] = { "select_next", "fallback_to_mappings" },
+      ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+      ["<Tab>"] = { "snippet_forward", "fallback" },
+      ["<S-Tab>"] = { "snippet_backward", "fallback" },
+      ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+    },
 
     appearance = {
       nerd_font_variant = "mono", -- "mono" | "normal" Nerd Font
