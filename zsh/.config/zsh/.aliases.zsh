b9a07ecc25a8f40f18f3b3d67107ffd251396fde
jah377 <jonathan.harris.eit@gmail.com>
2025-09-23 17:41:09 +0200

    Add cli aliases

--- zsh/.config/zsh/.aliases.zsh
+++ zsh/.config/zsh/.aliases.zsh
@@ -9,7 +9,8 @@ alias n="nvim"
 # Commands
 alias ls="eza -la --icons" # return list + hidden
 alias lst="eza -laT --icons" # return list-tree + hidden
-alias cdd="fuzzy_find_dirs" # jump to selected file
+alias cdd="fzf_dirs_to_cd $HOME"
+alias cdc="cd ~/dotfiles && nvim"
 alias ff="fzf --walker=file,hidden" # return file name
 
 # Directories
