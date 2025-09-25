ea2fdabed8e298180644d215a6417879fcf8fdf8
jah377 <jonathan.harris.eit@gmail.com>
2025-09-25 17:28:55 +0200

    Fix function name in aliases.sh

--- zsh/.config/zsh/.aliases.zsh
+++ zsh/.config/zsh/.aliases.zsh
@@ -9,7 +9,7 @@ alias n="nvim"
 # Commands
 alias ls="eza -la --icons" # return list + hidden
 alias lst="eza -laT --icons" # return list-tree + hidden
-alias cdd="fzf_dirs_to_cd $HOME"
+alias cdd="fzf_dirs_and_cd $HOME"
 alias cdc="cd ~/dotfiles && nvim"
 alias ff="fzf --walker=file,hidden" # return file name
 
