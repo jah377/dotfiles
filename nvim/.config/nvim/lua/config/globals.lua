e07811b345bcbab02fc23609a3c4e7ab1160c4d4
jah377 <jonathan.harris.eit@gmail.com>
2025-09-27 13:50:34 +0200

    Refactor globals to globals.lua

---
+++ nvim/.config/nvim/lua/config/globals.lua
@@ -0,0 +1,9 @@
+local global = vim.g
+
+-- Set <space> as leader key
+-- NOTE: Must happen before loading plugins.
+global.mapleader = " "
+global.maplocalleader = " "
+
+-- Use `nerd font` if installed and selected in terminal
+global.have_nerd_font = true
