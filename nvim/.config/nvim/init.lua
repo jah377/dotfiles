37de8a75acdd9f5d4328ba5694743b1f6b737525
jah377 <jonathan.harris.eit@gmail.com>
2025-09-27 13:51:39 +0200

    Update init.lua to include globals and lsp

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,21 +1,9 @@
--- init.lua
--- Main entry point for Neovim configuration
-
--- core settings
+-- Core settings
+require("config.globals")
 require("config.options")
 require("config.keymaps")
 require("config.autocmds")
+require("config.lsp")
 
--- 3rd party plugins
+-- 3rd-Party Plugins
 require("config.lazy")
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
