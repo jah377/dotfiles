d464f100ecbf339a3ec79fd4bad7b20a9a18c698
jah377 <jonathan.harris.eit@gmail.com>
2025-09-27 13:50:19 +0200

    Format autocmds.lua

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,7 +1,4 @@
--- autocmds.lua
--- Define custom autocommands
-
--- highlight text on yank
+-- Highlight text on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
   group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
