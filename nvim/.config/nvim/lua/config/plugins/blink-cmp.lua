fad92bf7bd5beb377fd6689bd4d559c7e13bc24f
jah377 <jonathan.harris.eit@gmail.com>
2025-09-28 12:29:25 +0200

    Refactor blink-cmp.lua

--- nvim/.config/nvim/lua/config/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/config/plugins/blink-cmp.lua
@@ -1,22 +1,29 @@
 return {
   "saghen/blink.cmp",
+  -- Snippets in ~/.local/share/nvim/lazy/friendly-snippets/snippets
   dependencies = { "rafamadriz/friendly-snippets" },
   version = "1.*", -- use stable release
-  --
+  event = { "LspAttach" },
+
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
-    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-    -- 'super-tab' for mappings similar to vscode (tab to accept)
-    -- 'enter' for enter to accept
-    -- 'none' for no mappings
-    --
-    -- All presets have the following mappings:
-    -- C-space: Open menu or open docs if already open
-    -- C-n/C-p or Up/Down: Select next/previous item
-    -- C-e: Hide menu
-    -- C-k: Toggle signature help (if signature.enabled = true)
-    --
+    appearance = { nerd_font_variant = "mono" },
+    fuzzy = { implementation = "prefer_rust_with_warning" },
+    completion = { documentation = { auto_show = false } },
+
+    -- From https://github.com/folke/lazydev.nvim
+    sources = {
+      default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+      providers = {
+        lazydev = {
+          name = "LazyDev",
+          module = "lazydev.integrations.blink",
+          score_offest = 100, -- lazydev completion top priority
+        },
+      },
+    },
+
     -- See :h blink-cmp-config-keymap for defining your own keymap
     keymap = {
       preset = "none",
@@ -34,20 +41,7 @@ return {
       ["<S-Tab>"] = { "snippet_backward", "fallback" },
       ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
     },
-
-    appearance = {
-      nerd_font_variant = "mono", -- "mono" | "normal" Nerd Font
-    },
-
-    -- (Default) Only show the documentation popup when manually triggered
-    completion = { documentation = { auto_show = false } },
-
-    sources = {
-      -- Excluded "buffer"
-      default = { "lsp", "path", "snippets" },
-    },
-
-    fuzzy = { implementation = "prefer_rust_with_warning" }, -- Default
   },
+  -- From https://cmp.saghen.dev/installation.html#lazy-nvim
   opts_extend = { "sources.default" },
 }
