fdd0ddd3afee2ef80921bc2b381e644dd9b4d7aa
jah377 <jonathan.harris.eit@gmail.com>
2025-09-28 12:28:37 +0200

    Add cmds to autocmds.lua

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,8 +1,58 @@
--- Highlight text on yank
-vim.api.nvim_create_autocmd("TextYankPost", {
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
   group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
+})
+
+autocmd("FileType", {
+  desc = "Automatically Split help Buffers to the right",
+  pattern = "help",
+  command = "wincmd L",
+})
+
+-- Show cursorline only on active windows
+-- From https://github.com/folke/dot/blob/master/nvim/lua/config/autocmds.lua
+autocmd({ "InsertLeave", "WinEnter" }, {
+  callback = function()
+    if vim.w.auto_cursorline then
+      vim.wo.cursorline = true
+      vim.w.auto_cursorline = false
+    end
+  end,
+})
+
+autocmd({ "InsertEnter", "WinLeave" }, {
+  callback = function()
+    if vim.wo.cursorline then
+      vim.w.auto_cursorline = true
+      vim.wo.cursorline = false
+    end
+  end,
+})
+
+-- Use 'q' to close special buffer types. '' catches a lot of transient plugin windows.
+-- From https://www.reddit.com/r/neovim/comments/1i2xw2m/share_your_favorite_autocmds/
+autocmd("FileType", {
+
+  pattern = {
+    "help",
+    "startuptime",
+    "qf",
+    "lspinfo",
+    "man",
+    "Oil",
+    "checkhealth",
+    "neotest-output-panel",
+    "neotest-summary",
+    "lazy",
+  },
+  command = [[
+          nnoremap <buffer><silent> q :close<CR>
+          nnoremap <buffer><silent> <ESC> :close<CR>
+          set nobuflisted
+      ]],
 })
