67dee7a9acc2742bbdd3b39c4a1a8b6d3f6c9993
jah377 <jonathan.harris.eit@gmail.com>
2025-09-28 12:28:20 +0200

    Add kbds to keymaps.lua

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,4 +1,9 @@
--- Package-specific kbd defined in `config/plugins/<plugin>.lua`
+-- ============================================================================
+-- TITLE: NeoVim keymaps
+-- ABOUT: sets some quality-of-life keymaps
+--   - Package-specific kbd defined in `~/.config/plugins/<plugin>.lua`
+-- ============================================================================
+
 local kmap = vim.keymap
 
 --  Use CTRL+<hjkl> to switch between windows
@@ -8,22 +13,21 @@ kmap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" }
 kmap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 kmap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+-- Center screen when jumping
+vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result (centered)" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search result (centered)" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half page down (centered)" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half page up (centered)" })
+
 -- Manipulate buffers
 kmap.set("n", "<leader>bn", ":bn<cr>", { desc = "[B]uffer [N]ext" })
 kmap.set("n", "<leader>bp", ":bp<cr>", { desc = "[B]uffer [P]revious" })
 kmap.set("n", "<leader>bd", ":bd<cr>", { desc = "[B]uffer [D]elete" })
-kmap.set("n", "<leader>bw", ":w<CR>", { desc = "Write buffer" })
-kmap.set("n", "<leader>bW", ":wq<CR>", { desc = "Write and quit buffer" })
-kmap.set("n", "<leader>bq", ":q<CR>", { desc = "Quit buffer" })
-kmap.set("n", "<leader>bq", ":q!<CR>", { desc = "Force Quit buffer" })
+
+-- Enable re-indenting; by default, indenting exists visual mode
+vim.keymap.set("v", "<", "<gv", { desc = "Indent left  and reselect" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect" })
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 kmap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-
--- Quickly execute file at point
-kmap.set("n", "<leader>x", function()
-  vim.cmd("write")
-  vim.cmd("luafile %")
-  print("Executed current Lua file!")
-end, { desc = "Run current Lua file" })
