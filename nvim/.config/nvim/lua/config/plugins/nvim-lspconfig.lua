c3b7c422ce9115e86bface814cf0c3db08a900fc
jah377 <jonathan.harris.eit@gmail.com>
2025-09-30 20:42:21 +0200

    Add nvim-lspconfig.lua

---
+++ nvim/.config/nvim/lua/config/plugins/nvim-lspconfig.lua
@@ -0,0 +1,29 @@
+-- ================================================================================================
+-- TITLE : nvim-lspconfig
+--
+-- ABOUT : Quickstart configurations for the built-in Neovim LSP client.
+--
+-- LINKS :
+--   > github                  : https://github.com/neovim/nvim-lspconfig
+--   > mason.nvim (dep)        : https://github.com/mason-org/mason.nvim
+--   > cmp-nvim-lsp (dep)      : https://github.com/hrsh7th/cmp-nvim-lsp
+--
+-- FILES :
+--   > lsp_servers/            : Contains server-specific setting files
+--   > lsp_servers/init.lua    : Enables servers
+--   > utils/on_attach.lua     : Configure buffer-local LSP setup
+--   > utils/diagnostics.lua   : Configure LSP diagnostics
+--
+-- ================================================================================================
+
+return {
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    { "mason-org/mason.nvim", opts = {} }, -- LSP/DAP/Linter installer & manager
+    "hrsh7th/cmp-nvim-lsp", -- nvim-cmp source for LSP-based completion
+  },
+  config = function()
+    -- require("utils.diagnostics").setup()
+    require("config.lsp_servers") -- See lsp/init.lua
+  end,
+}
