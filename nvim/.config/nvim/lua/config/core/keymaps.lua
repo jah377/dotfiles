1093e1acd531e8792b0459d86c2b074378887b0e
jah377 <jonathan.harris.eit@gmail.com>
2025-11-03 22:43:07 +0100

    Update keymaps.lua

--- nvim/.config/nvim/lua/config/core/keymaps.lua
+++ nvim/.config/nvim/lua/config/core/keymaps.lua
@@ -7,30 +7,30 @@
 --
 -- ============================================================================
 
-local keymap = vim.keymap
+local kbd = vim.keymap.set
 
 --  Use CTRL+<hjkl> to switch between windows
 --  See `:help wincmd` for a list of all window commands
-keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+kbd("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+kbd("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+kbd("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+kbd("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- Center screen when jumping
-vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result (centered)" })
-vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search result (centered)" })
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half page down (centered)" })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half page up (centered)" })
+kbd("n", "n", "nzzzv", { desc = "Next search result (centered)" })
+kbd("n", "N", "Nzzzv", { desc = "Previous search result (centered)" })
+kbd("n", "<C-d>", "<C-d>zz", { desc = "Half page down (centered)" })
+kbd("n", "<C-u>", "<C-u>zz", { desc = "Half page up (centered)" })
 
 -- Manipulate buffers
-keymap.set("n", "<leader>bn", ":bn<cr>", { desc = "[B]uffer [N]ext" })
-keymap.set("n", "<leader>bp", ":bp<cr>", { desc = "[B]uffer [P]revious" })
-keymap.set("n", "<leader>bd", ":bd<cr>", { desc = "[B]uffer [D]elete" })
+kbd("n", "<leader>bn", ":bn<cr>", { desc = "[B]uffer [N]ext" })
+kbd("n", "<leader>bp", ":bp<cr>", { desc = "[B]uffer [P]revious" })
+kbd("n", "<leader>bd", ":bd<cr>", { desc = "[B]uffer [D]elete" })
 
--- Enable re-indenting; by default, indenting exists visual mode
-vim.keymap.set("v", "<", "<gv", { desc = "Indent left  and reselect" })
-vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect" })
+-- Enable re-indenting; by default, '<' indents and deselects
+kbd("v", "<", "<gv", { desc = "Indent left  and reselect" })
+kbd("v", ">", ">gv", { desc = "Indent right and reselect" })
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
-keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+kbd("n", "<Esc>", "<cmd>nohlsearch<CR>")
