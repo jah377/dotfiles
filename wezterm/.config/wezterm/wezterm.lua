398347ab325fbc14de058898d11c9ed119fb9731
jah377 <jonathan.harris.eit@gmail.com>
2025-12-11 16:28:48 +0100

    Fix wezterm config to use fzf keybindings

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -8,13 +8,17 @@ config = {
   window_close_confirmation = "NeverPrompt",
   window_decorations = "RESIZE", -- disable title bar; able to resize window
   window_padding = {
-    left = 5,
+    left = 10,
     right = 5,
     top = 5,
     bottom = 0,
   },
   default_cursor_style = "SteadyBlock",
   color_scheme = "Catppuccin Mocha",
+
+  -- Must set Option key to act as normal Alt key as we wish to use 'fzf' kbds
+  send_composed_key_when_left_alt_is_pressed = false,
+  send_composed_key_when_right_alt_is_pressed = false,
 
   -- wezterm ls-fonts --list-system | grep "JetBrains"
   font = wezterm.font("JetBrains Mono", { weight = "Regular" }),
@@ -55,7 +59,7 @@ config = {
   },
   keys = {
     {
-      -- Disable; instead used for smart selection in treesitter.lua
+      -- Disable 'C-=' as kbd used for smart selection in treesitter.lua
       key = "=",
       mods = "CTRL",
       action = wezterm.action.DisableDefaultAssignment,
