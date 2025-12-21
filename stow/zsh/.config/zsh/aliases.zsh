f0a4e4e7b6983f42c20b12e93c925a6777b947a3
jah377 <jonathan.harris.eit@gmail.com>
2025-12-21 15:43:22 +0100

    Update eza aliases

--- stow/zsh/.config/zsh/aliases.zsh
+++ stow/zsh/.config/zsh/aliases.zsh
@@ -2,8 +2,10 @@
 alias lg="lazygit"
 alias n="nvim"
 alias cd=z # use zoxide instead
-alias ls="eza -la --icons --group-directories-first"
-alias lst="eza -laT --icons --group-directories-first --no-permissions --no-filesize --no-user --no-time --git-ignore"
+
+MY_EZA_DEFAULTS="--icons --group-directories-first --no-user --git"
+alias ls="eza -la $MY_EZA_DEFAULTS"
+alias lst="eza -laT $MY_EZA_DEFAULTS --no-permissions --no-filesize --no-user --no-time --git-ignore"
 
 # Nice to have
 alias src="source .venv/bin/activate" # activate python .venv
