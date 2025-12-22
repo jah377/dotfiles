b9f590b6d83f56113d574c6553ea7f9318d53240
jah377 <jonathan.harris.eit@gmail.com>
2025-12-22 22:12:58 +0100

    feat(nvim): add auto-commands.

--- stow/nvim/.config/nvim/lua/config/core/autocmds.lua
+++ stow/nvim/.config/nvim/lua/config/core/autocmds.lua
@@ -121,3 +121,143 @@ autocmd("BufWritePre", {
     vim.fn.setpos(".", save_cursor)
   end,
 })
+
+-- Auto-resize splits when window is resized
+autocmd("VimResized", {
+  desc = "Resize splits when window is resized",
+  callback = function()
+    vim.cmd("tabdo wincmd =")
+  end,
+})
+
+-- Use absolute line numbers in insert mode, relative in normal mode
+local number_toggle_group = vim.api.nvim_create_augroup("NumberToggle", {})
+autocmd({ "BufEnter", "FocusGained", "InsertLeave", "WinEnter" }, {
+  group = number_toggle_group,
+  callback = function()
+    if vim.o.number and vim.api.nvim_get_mode().mode ~= "i" then
+      vim.opt.relativenumber = true
+    end
+  end,
+})
+
+autocmd({ "BufLeave", "FocusLost", "InsertEnter", "WinLeave" }, {
+  group = number_toggle_group,
+  callback = function()
+    if vim.o.number then
+      vim.opt.relativenumber = false
+    end
+  end,
+})
+
+-- Make shell scripts executable on save
+autocmd("BufWritePost", {
+  desc = "Make shell scripts executable",
+  pattern = { "*.sh", "*.zsh", "*.bash" },
+  callback = function()
+    local file = vim.fn.expand("%:p")
+    vim.fn.system({ "chmod", "+x", file })
+  end,
+})
+
+-- Terminal settings
+autocmd("TermOpen", {
+  desc = "Terminal settings",
+  callback = function()
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
+    vim.opt_local.signcolumn = "no"
+    vim.opt_local.scrolloff = 0
+  end,
+})
+
+-- Start in insert mode when entering terminal
+autocmd("BufEnter", {
+  pattern = "term://*",
+  command = "startinsert",
+})
+
+-- Disable features for large files (>1MB)
+autocmd("BufReadPre", {
+  desc = "Disable certain functionality for large files",
+  callback = function(args)
+    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(args.buf))
+    if ok and stats and stats.size > 1000000 then -- 1MB
+      vim.b.large_file = true
+      vim.opt_local.spell = false
+      vim.opt_local.swapfile = false
+      vim.opt_local.undofile = false
+      vim.opt_local.breakindent = false
+      vim.opt_local.colorcolumn = ""
+      vim.opt_local.statuscolumn = ""
+      vim.opt_local.signcolumn = "no"
+      vim.opt_local.foldcolumn = "0"
+      vim.opt_local.winbar = ""
+      vim.cmd("syntax clear")
+    end
+  end,
+})
+
+-- Close quickfix window if it's the last window
+autocmd("WinEnter", {
+  desc = "Close quickfix if it's the last window",
+  callback = function()
+    if vim.fn.winnr("$") == 1 and vim.bo.buftype == "quickfix" then
+      vim.cmd("q")
+    end
+  end,
+})
+
+-- Highlight TODO, FIXME, NOTE, etc.
+autocmd({ "BufEnter", "BufWritePost" }, {
+  desc = "Highlight TODO comments",
+  callback = function()
+    vim.fn.matchadd("Todo", "\\(TODO\\|FIXME\\|NOTE\\|HACK\\|PERF\\):")
+  end,
+})
+
+-- Go to last location in quickfix/location list
+autocmd("FileType", {
+  pattern = "qf",
+  callback = function()
+    vim.cmd("normal! G")
+  end,
+})
+
+-- Automatically disable paste mode when leaving insert mode
+autocmd("InsertLeave", {
+  desc = "Disable paste mode on leaving insert",
+  callback = function()
+    if vim.o.paste then
+      vim.o.paste = false
+    end
+  end,
+})
+
+-- Highlight URLs
+autocmd({ "VimEnter", "FileType", "BufEnter", "WinEnter" }, {
+  desc = "URL Highlighting",
+  callback = function()
+    vim.fn.matchadd("URL", "\\v\\c%(%(h?ttps?|ftp|file|ssh|git)://|[a-z]+[@][a-z]+[.][a-z]+:)%([&:#*@~%_\\-=?!+;/0-9a-z]+%(%([.;/?]|[.][.]+)[&:#*@~%_\\-=?!+/0-9a-z]+|:\\d+|,%(%(%(h?ttps?|ftp|file|ssh|git)://|[a-z]+[@][a-z]+[.][a-z]+:)@![0-9a-z]+))*|\\([&:#*@~%_\\-=?!+;/.0-9a-z]*\\)|\\[[&:#*@~%_\\-=?!+;/.0-9a-z]*\\]|\\{%([&:#*@~%_\\-=?!+;/.0-9a-z]*|\\{[&:#*@~%_\\-=?!+;/.0-9a-z]*})\\})+")
+  end,
+})
+
+-- Set filetype for common config files
+autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { ".env*", "*.conf", "config" },
+  command = "setfiletype conf",
+})
+
+autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { "*.md", "*.markdown" },
+  command = "setfiletype markdown",
+})
+
+-- Git commit message settings
+autocmd("FileType", {
+  pattern = "gitcommit",
+  callback = function()
+    vim.opt_local.spell = true
+    vim.opt_local.colorcolumn = "72"
+  end,
+})
