fa4a47d245c81e9c91d1eb4f55ac595fd1a01981
jah377 <jonathan.harris.eit@gmail.com>
2025-12-23 14:51:53 +0100

    Refactor keymaps

--- stow/nvim/.config/nvim/lua/config/core/keymaps.lua
+++ stow/nvim/.config/nvim/lua/config/core/keymaps.lua
@@ -32,20 +32,8 @@ kbd("v", ">", ">gv", { desc = "Indent right and reselect" })
 --  See `:help hlsearch`
 kbd("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
--- Move lines up/down
-kbd("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
-kbd("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
-kbd("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
-kbd("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
-
--- Better paste (don't yank replaced text)
-kbd("x", "<leader>p", '"_dP', { desc = "Paste without yanking" })
-
 -- Quickfix navigation
 kbd("n", "<leader>qn", ":cnext<CR>", { desc = "[Q]uickfix [N]ext" })
 kbd("n", "<leader>qp", ":cprev<CR>", { desc = "[Q]uickfix [P]revious" })
 kbd("n", "<leader>qo", ":copen<CR>", { desc = "[Q]uickfix [O]pen" })
 kbd("n", "<leader>qc", ":cclose<CR>", { desc = "[Q]uickfix [C]lose" })
-
--- Save with Ctrl-S
-kbd({ "n", "i", "v" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
