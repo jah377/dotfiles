0f148826a8a65283ec805a64651944d8793780a3
jah377 <jonathan.harris.eit@gmail.com>
2025-12-23 15:57:17 +0100

    Refactor vim-tmux-navigator.lua

--- stow/nvim/.config/nvim/lua/config/plugins/vim-tmux-navigator.lua
+++ stow/nvim/.config/nvim/lua/config/plugins/vim-tmux-navigator.lua
@@ -1,6 +1,15 @@
+-- [[ Seamless navigation between tmux panes and vim splits ]]
+-- Requires corresponding tmux configuration
+-- See: ~/.config/tmux/remappings.conf
+-- See: https://github.com/christoomey/vim-tmux-navigator
+
 return {
   "christoomey/vim-tmux-navigator",
   lazy = false, -- load immediately
+  init = function()
+    -- Keep tmux window zoomed when moving from Vim to another pane
+    vim.g.tmux_navigator_preserve_zoom = 1
+  end,
   keys = {
     { "<C-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
     { "<C-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
