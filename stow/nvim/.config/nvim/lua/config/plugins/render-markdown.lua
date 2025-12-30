f7c381105f509649cab5496831f2a29450d34105
jah377 <jonathan.harris.eit@gmail.com>
2025-12-30 15:53:59 +0100

    Fix mini dependency in render-markdown.lua

--- stow/nvim/.config/nvim/lua/config/plugins/render-markdown.lua
+++ stow/nvim/.config/nvim/lua/config/plugins/render-markdown.lua
@@ -4,7 +4,7 @@
 return {
   {
     "MeanderingProgrammer/render-markdown.nvim",
-    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.icons" },
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" },
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {},
