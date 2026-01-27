8bccc60af349c1cb32dff285fca17b5918327497
jah377 <jonathan.harris.eit@gmail.com>
2026-01-27 08:33:21 +0100

    Add doc to vimtex.lua

--- stow/nvim/.config/nvim/lua/config/plugins/vimtex.lua
+++ stow/nvim/.config/nvim/lua/config/plugins/vimtex.lua
@@ -6,7 +6,11 @@ return {
   -- No need as ftplugins only loaded once file type is opened
   lazy = false,
   init = function()
-    vim.g.vimtex_view_method = "skim" -- zathura
-    vim.g.vimtext_compiler_method = "latexmk" -- default
+    -- Cannot install `zathura` via brew. `Preview` doesn't support `SyncTex`,
+    -- forwrd/inverse search between source and PDF. Use `skim` instead.
+    vim.g.vimtex_view_method = "skim" -- default: zathura
+
+    -- `brew install --cask mactex` to intsall `latexmk`
+    vim.g.vimtext_compiler_method = "latexmk"
   end,
 }
