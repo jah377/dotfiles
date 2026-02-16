e7dc0812d9b3d9846a1b1426da459358efc6c543
jah377 <jonathan.harris.eit@gmail.com>
2026-02-16 22:46:42 +0100

    Drop python.lua settings

--- stow/nvim/.config/nvim/after/ftplugin/python.lua
+++ stow/nvim/.config/nvim/after/ftplugin/python.lua
@@ -4,8 +4,6 @@ set.spell = true -- catch typos in comments and docstrings
 set.shiftwidth = 4 -- PEP 8: use 4 spaces per indentation level
 set.tabstop = 4 -- display tabs as 4 spaces
 set.softtabstop = 4 -- backspace removes 4 spaces at once
-set.textwidth = 88 -- black/ruff default line length
-set.colorcolumn = "89" -- visual guide at line limit
 
 -- Automatically capitalize boolean values
 vim.cmd.inoreabbrev("<buffer> true True")
