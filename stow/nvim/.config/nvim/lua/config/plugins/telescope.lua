b1b1880610fb5c76f6d19aae26eb34eb2a8b1ea9
Jon Harris <jharris@jharris.local>
2026-05-18 22:50:01 +0200

    Add fff.lua

--- stow/nvim/.config/nvim/lua/config/plugins/telescope.lua
+++ stow/nvim/.config/nvim/lua/config/plugins/telescope.lua
@@ -58,35 +58,20 @@ return {
       end
     end
 
+    --- See fff.lua for additional search tooling
     keymap.set("n", "<leader>fh", open_vertical(builtin.help_tags), { desc = "Find Help" })
     keymap.set("n", "<leader>fm", open_vertical(builtin.man_pages), { desc = "Find Man-Pages" })
     keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Find Keymaps" })
     keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Find Diagnostics" })
     keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find Buffers" })
-    keymap.set("n", "<leader>fp", builtin.git_files, { desc = "Find Git Files" })
     keymap.set("n", "<leader>ft", builtin.treesitter, { desc = "Find Treesitter Objects" })
     keymap.set("n", "<leader>fr", builtin.resume, { desc = "Resume Search" })
     keymap.set("n", "<leader>fz", builtin.current_buffer_fuzzy_find, { desc = "Fuzzy Find in Buffer" })
-    keymap.set("n", "<leader>ff", function()
-      builtin.find_files({ hidden = true })
-    end, { desc = "Find All Files" })
-    keymap.set("n", "<leader>fc", function()
-      builtin.find_files({ cwd = "~/dotfiles/", hidden = true })
-    end, { desc = "Find Config Files" })
     keymap.set("n", "<space>fL", function()
       builtin.find_files({ cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy") })
     end, { desc = "Find Lazy Plugin Files" })
 
     -- Markdown navigation (telescope-heading)
     keymap.set("n", "<leader>mh", "<cmd>Telescope heading<CR>", { desc = "[M]arkdown [H]eadings" })
-
-    -- NOTE: Grep-related functions requires `ripgrep` (see brew.sh)
-    keymap.set("n", "<leader>fgs", builtin.grep_string, { desc = "Grep: Strings" })
-    keymap.set("n", "<leader>fgd", function()
-      builtin.live_grep({ grep_open_files = false, prompt_title = "Live Grep in Directory" })
-    end, { desc = "Grep: Directory Files" })
-    keymap.set("n", "<leader>fgo", function()
-      builtin.live_grep({ grep_open_files = true, prompt_title = "Live Grep in Open Files" })
-    end, { desc = "Grep: Open Files" })
   end,
 }
