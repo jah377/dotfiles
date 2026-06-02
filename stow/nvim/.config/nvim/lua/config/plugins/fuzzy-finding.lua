7473f41f46b39998b8eaf446c7096ae027b477f3
Jon Harris <jharris@jharris.local>
2026-06-02 13:08:44 +0200

    Fix fuzzy-finding.lua

--- stow/nvim/.config/nvim/lua/config/plugins/fuzzy-finding.lua
+++ stow/nvim/.config/nvim/lua/config/plugins/fuzzy-finding.lua
@@ -2,8 +2,8 @@
 -- FILE: lua/config/plugins/fuzzy_finding.lua
 -- Fuzzy finder and file search.
 --
--- fff.nvim is the entry point; telescope.nvim is declared as a dependency so
--- both plugins load together and share a single config function.
+-- telescope.nvim and fff.nvim are declared as independent specs so that
+-- telescope fully initialises before fff patches its picker internals.
 --
 -- DOCUMENTATION:
 --  > Github : https://github.com/nvim-telescope/telescope.nvim
@@ -12,43 +12,32 @@
 --  > :help telescope.builtin
 -- =============================================================================
 
-return {
-  "dmtrKovalenko/fff.nvim",
+-- ---------------------------------------------------------------------------
+-- Telescope
+-- ---------------------------------------------------------------------------
+local telescope_spec = {
+  "nvim-telescope/telescope.nvim",
   dependencies = {
-    "nvim-lua/plenary.nvim", -- required by telescope
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" }, -- faster fuzzy sorting via native FZF
-    { "nvim-telescope/telescope-ui-select.nvim" }, -- replaces vim.ui.select() with telescope dropdown
-    { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font }, -- file icons (requires Nerd Font)
-    { "crispgm/telescope-heading.nvim" }, -- jump to markdown headings
-    "nvim-telescope/telescope.nvim", -- listed last so its deps resolve first
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    { "nvim-telescope/telescope-ui-select.nvim" },
+    { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+    { "crispgm/telescope-heading.nvim" },
   },
-  build = function()
-    -- downloads a prebuilt binary or falls back to cargo build
-    require("fff.download").download_or_build_binary()
-  end,
-  -- for nixos:
-  -- build = "nix run .#release",
   event = "VimEnter",
-  lazy = false, -- fff self-manages its lazy initialisation internally
-  opts = {
-    debug = { enabled = true, show_scores = true },
-  },
   config = function()
-    -- Display ui-select as a dropdown menu theme
     require("telescope").setup({
       extensions = { ["ui-select"] = { require("telescope.themes").get_dropdown() } },
     })
 
-    -- Load extensions (without error) if installed
     pcall(require("telescope").load_extension, "fzf")
     pcall(require("telescope").load_extension, "ui-select")
     pcall(require("telescope").load_extension, "heading")
 
-    local telescope = require("telescope.builtin")
-    local fff = require("fff")
+    local builtin = require("telescope.builtin")
     local keymap = vim.keymap
 
-    -- Helper function creates picker to open file to right
+    -- Helper: wraps a picker so <CR> opens in a vertical split
     local function open_vertical(picker_fn)
       return function()
         local actions = require("telescope.actions")
@@ -62,21 +51,39 @@ return {
       end
     end
 
-    -- Telescope keymaps
-    keymap.set("n", "<leader>fh", open_vertical(telescope.help_tags), { desc = "Find Help" })
-    keymap.set("n", "<leader>fm", open_vertical(telescope.man_pages), { desc = "Find Man-Pages" })
-    keymap.set("n", "<leader>fk", telescope.keymaps, { desc = "Find Keymaps" })
-    keymap.set("n", "<leader>fd", telescope.diagnostics, { desc = "Find Diagnostics" })
-    keymap.set("n", "<leader>fb", telescope.buffers, { desc = "Find Buffers" })
-    keymap.set("n", "<leader>ft", telescope.treesitter, { desc = "Find Treesitter Objects" })
-    keymap.set("n", "<leader>fr", telescope.resume, { desc = "Resume Search" })
-    keymap.set("n", "<leader>fz", telescope.current_buffer_fuzzy_find, { desc = "Fuzzy Find in Buffer" })
+    keymap.set("n", "<leader>fh", open_vertical(builtin.help_tags), { desc = "Find Help" })
+    keymap.set("n", "<leader>fm", open_vertical(builtin.man_pages), { desc = "Find Man-Pages" })
+    keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Find Keymaps" })
+    keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Find Diagnostics" })
+    keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find Buffers" })
+    keymap.set("n", "<leader>ft", builtin.treesitter, { desc = "Find Treesitter Objects" })
+    keymap.set("n", "<leader>fr", builtin.resume, { desc = "Resume Search" })
+    keymap.set("n", "<leader>fz", builtin.current_buffer_fuzzy_find, { desc = "Fuzzy Find in Buffer" })
     keymap.set("n", "<space>fL", function()
-      telescope.find_files({ cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy") })
+      builtin.find_files({ cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy") })
     end, { desc = "Find Lazy Plugin Files" })
     keymap.set("n", "<leader>mh", "<cmd>Telescope heading<CR>", { desc = "[M]arkdown [H]eadings" })
+  end,
+}
 
-    -- fff keymaps
+-- ---------------------------------------------------------------------------
+-- fff.nvim
+-- ---------------------------------------------------------------------------
+local fff_spec = {
+  "dmtrKovalenko/fff.nvim",
+  dependencies = { "nvim-telescope/telescope.nvim" },
+  build = function()
+    require("fff.download").download_or_build_binary()
+  end,
+  lazy = false,
+  opts = {
+    debug = { enabled = true, show_scores = true },
+  },
+  config = function(_, opts)
+    local fff = require("fff")
+    fff.setup(opts)
+
+    local keymap = vim.keymap
     keymap.set("n", "<leader>ff", fff.find_files, { desc = "FFFind files" })
     keymap.set("n", "<leader>fc", function()
       fff.find_files_in_dir("~/dotfiles")
@@ -87,3 +94,5 @@ return {
     end, { desc = "FFFind current word" })
   end,
 }
+
+return { telescope_spec, fff_spec }
