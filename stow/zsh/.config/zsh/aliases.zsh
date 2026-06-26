a20b29d3798585c4f2d49e2dedd92a32fe2a705f
Jon Harris <jharris@jharris.local>
2026-06-26 08:35:30 +0200

    refactor(zsh): rename eza defaults variable to lowercase and unset after use

--- stow/zsh/.config/zsh/aliases.zsh
+++ stow/zsh/.config/zsh/aliases.zsh
@@ -3,9 +3,10 @@ alias lg="lazygit"
 alias n="nvim"
 alias cd=z # use zoxide instead
 
-MY_EZA_DEFAULTS="--icons --group-directories-first --no-user --git"
-alias ls="eza -la $MY_EZA_DEFAULTS"
-alias lst="eza -laT $MY_EZA_DEFAULTS --no-permissions --no-filesize --no-user --no-time --git-ignore"
+_eza_defaults="--icons --group-directories-first --no-user --git"
+alias ls="eza -la $_eza_defaults"
+alias lst="eza -laT $_eza_defaults --no-permissions --no-filesize --no-time --git-ignore"
+unset _eza_defaults
 
 alias src="source .venv/bin/activate" # activate python .venv
 alias py="source .venv/bin/activate && ipython"
