a40066936730918c24374de716e16e69cfbefbd7
Jon Harris <jharris@jharris.local>
2026-06-26 08:34:10 +0200

    fix(scripts): select appropriate github SSH host based on machine type

--- scripts/zettelkasten.sh
+++ scripts/zettelkasten.sh
@@ -4,7 +4,12 @@ set -euo pipefail
 # Script Name: clone_notes.sh
 # Description: Clone notes repo locally
 
-git clone git@github.com-personal:jah377/zettelkasten.git ~/zettelkasten
+# Work machines use a "github.com-personal" SSH host alias to distinguish keys
+if [[ "${IS_WORK_MACHINE}" == "true" ]]; then
+  git clone git@github.com-personal:jah377/zettelkasten.git ~/zettelkasten
+else
+  git clone git@github.com:jah377/zettelkasten.git ~/zettelkasten
+fi
 
 # Set local config for notes repo
 git -C ~/zettelkasten config --local user.name "jah377"
