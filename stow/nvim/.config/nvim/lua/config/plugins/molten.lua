8c1fb52163f5fcfa4c134af86be8d4dabceb3d9c
Jon Harris <jharris@jharris.local>
2026-07-23 09:06:05 +0200

    fix(molten): resolve weird behavior with plots in virtual text output

--- stow/nvim/.config/nvim/lua/config/plugins/molten.lua
+++ stow/nvim/.config/nvim/lua/config/plugins/molten.lua
@@ -171,8 +171,8 @@ return {
     init = function()
       vim.g.molten_image_provider = "image.nvim" -- use image.nvim for inline image rendering
       vim.g.molten_wrap_output = true
-      vim.g.molten_virt_text_output = true -- render output as virtual text below cell
-      vim.g.molten_auto_open_output = false -- use <leader>jo explicitly
+      vim.g.molten_virt_text_output = false -- disabled; weird behavior with plots
+      vim.g.molten_auto_open_output = true -- use <leader>jo explicitly
       vim.g.molten_output_win_max_height = 40
       vim.g.molten_virt_lines_off_by_1 = true
       vim.g.molten_use_border_highlights = true
